8ea3b656a514b23c9af918c5dd6a6a83d4bd792c
tajaa <tajatheprince@gmail.com>
2024-07-06 15:39:54 -0700

    brain storm

--- index.mjs
+++ index.mjs
@@ -8,8 +8,44 @@ const app = new Slack.App({
   token: process.env.SLACK_BOT_TOKEN,
 });
 
+const blocks = [
+  {
+    type: "section",
+    text: {
+      type: "mrkdwn",
+      text: "*BrainStorm* ⚡",
+    },
+  },
+  {
+    type: "divider",
+  },
+  {
+    type: "section",
+    text: {
+      type: "mrkdwn",
+      text: "Brainstorm!",
+    },
+  },
+  {
+    type: "actions",
+    elements: [
+      {
+        type: "button",
+        text: {
+          type: "plain_text",
+          text: "⚡",
+          emoji: true,
+        },
+        value: "lightning_1",
+        action_id: "lightning_button_1",
+      },
+    ],
+  },
+];
+
 await app.client.chat.postMessage({
   token: process.env.SLACK_BOT_TOKEN,
   channel: process.env.SLACK_CHANNEL,
   text: "this is a test",
+  blocks: blocks,
 });
